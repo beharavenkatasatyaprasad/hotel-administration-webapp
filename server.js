b05c98b6b50bc58711ea062fea3f0c7f1a70f3a3
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2020-11-13 17:07:15 +0530

    commit

--- server.js
+++ server.js
@@ -9,7 +9,23 @@ app.use(express.static('public'));
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false})
 
-const rooms = [];
+const rooms = [
+    {
+      id: 1,
+      roomnum: '602',
+      name: 'starindia',
+      aminities: [ 'AC', 'cateringservice', 'wifi' ],
+      fare: 'Rupees 900/- Only'
+    },
+    {
+      id: 2,
+      roomnum: '302',
+      name: 'starhood',
+      aminities: [ 'AC', 'cateringservice', 'wifi', 'hotwater' ],
+      fare: 'Rupees 600/- Only'
+    }
+  ]
+const customer_bookings=[];
 
 app.set('view engine','ejs');
 
@@ -52,19 +68,70 @@ app.post('/createroom',urlencodedParser,[
             fare : "Rupees "+req.body.fare+"/- Only"
         };
         rooms.push(newRoom);
+        // console.log(rooms)
         res.render('createroom',{
             newRoom
         })
     }
-
+    
 })
+
 
 app.get('/bookroom',(req,res) => {
-    res.render('bookroom')
+    res.render('bookroom',{
+        rooms
+    })
 })
 
-app.get('/listallcustomers',(req,res) => {
-    res.render('listallcustomers')
+app.post('/bookroom',urlencodedParser,[
+
+    // check('roomnum' ,"please select room number")
+    // .notEmpty(),
+    check('custname' ,"Please enter customer name")
+    .notEmpty(),
+    check('checkin_date' ,"Please select check-in date")
+    .notEmpty(),
+    check('checkin_time' ,"Please select check-in time")
+    .notEmpty(),
+    check('checkout_date' ,"Please select check-out date")
+    .notEmpty(),
+    check('checkout_time' ,"Please select check-out time")
+    .notEmpty()
+],(req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        //   return res.status(400).json({ errors: errors.array() });
+        const alert = errors.array()
+        res.render('bookroom',{
+            alert
+        })
+    }
+    else{
+        const newBooking = {
+            id : customer_bookings.length +1,
+            roomnum : req.body.roomnum,
+            custname : req.body.custname,
+            check_in : {
+                time : req.body.checkin_time,
+                date : req.body.checkin_date,
+            },
+            check_out : {
+                time : req.body.checkout_time,
+                date : req.body.checkout_date,
+            }
+        };
+        customer_bookings.push(newBooking);
+        res.render('bookroom',{
+            newBooking          
+        })
+    }
+    
+})
+app.get('/listallrooms',(req,res) => {
+    res.render('listallrooms',{
+        rooms
+    })
 })
 
 app.get('/listallbookings',(req,res) => {
