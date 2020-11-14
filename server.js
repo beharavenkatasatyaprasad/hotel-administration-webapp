bd7b84120b7298b89128b9073ba74d8c6887b1fd
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2020-11-14 19:57:13 +0530

    commit

--- server.js
+++ server.js
@@ -1,37 +1,104 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port =process.env.PORT || 3000 
-// app.use(express.json());
-const { check, validationResult } = require('express-validator');
-app.use
-app.use(express.static('public'));
-
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+// const methodOverride = require('method-override')
 const urlencodedParser = bodyParser.urlencoded({ extended: false})
+const { check, validationResult } = require('express-validator');
+app.use(express.urlencoded({ extended: false}))
+const initializePassport = require('./passport-config')
+const port =process.env.PORT || 3000 
 
-const rooms = [
-    {
-      id: 1,
-      roomnum: '602',
-      name: 'starindia',
-      aminities: [ 'AC', 'cateringservice', 'wifi' ],
-      fare: 'Rupees 900/- Only'
-    },
-    {
-      id: 2,
-      roomnum: '302',
-      name: 'starhood',
-      aminities: [ 'AC', 'cateringservice', 'wifi', 'hotwater' ],
-      fare: 'Rupees 600/- Only'
-    }
-  ]
+const users = []
+app.use(express.static('public'));
+const rooms = []
 const customer_bookings=[];
-
+    
 app.set('view engine','ejs');
-
+    
 app.get('/',function (req,res){
-    res.render('index');
+        // res.render('index');
+    res.redirect('/login')
 })
+    
+app.get('/login',(req,res) =>{
+    res.render('login');
+})
+   
+app.post('/login',urlencodedParser,[
+
+    check('username' ,"Username Required")
+    .notEmpty(),
+    
+    check('password' ,"Password Required")
+    .notEmpty(),
+    
+    
+    ],(req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        
+    const alert = errors.array()
+    res.render('login',{
+        alert
+     })
+    }
+    else{
+        const userlogin = {
+            username : req.body.username,
+            password : req.body.password
+        };
+        // console.log(userlogin)
+        const successMsg = 'Login Successfull'
+        res.render('login',{
+            successMsg
+        })
+    }
+    
+})
+
+app.get('/register',(req,res) =>{
+    
+       res.render('register')
+    })
+    
+app.post('/register',urlencodedParser,[
+
+    check('username' ,"Username Required")
+    .notEmpty(),
+    
+    check('password' ,"Password Required")
+    .notEmpty(),
+    
+    ],(req, res) => {
+    
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        
+    const alert = errors.array()
+    res.render('register',{
+        alert
+     })
+    }
+    else{
+        const newuser = {
+            username : req.body.username,
+            password : req.body.password
+        };
+        users.push(newuser);
+        const successMsg = 'Registration Successfull'
+        // console.log(newuser)
+        res.render('register',{
+            successMsg
+        })
+    }
+    
+})
+
 
 app.get('/createroom',(req,res) => {
     res.render('createroom')
